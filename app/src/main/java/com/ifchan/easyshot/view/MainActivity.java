67bdda6e543a7daf8b052c83c130270c202a0e3a
Chen-Yufeng <956472367@qq.com>
2018-07-09 15:53:07 +0800

    add WRITE_EXTERNALSTORAGE permission

---
+++ app/src/main/java/com/ifchan/easyshot/view/MainActivity.java
@@ -0,0 +1,54 @@
+package com.ifchan.easyshot.view;
+
+import android.Manifest;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.ifchan.easyshot.R;
+
+import java.util.List;
+
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
+
+public class MainActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks {
+    public static final String[] PERMISSIONS = {
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+    public static final int REQUEST_CODE = 1001;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        requestPermission();
+    }
+
+    private void requestPermission() {
+        if (!EasyPermissions.hasPermissions(this, PERMISSIONS)) {
+            EasyPermissions.requestPermissions(this, "Need write sdcard permission to save " +
+                    "screenshot.", REQUEST_CODE, PERMISSIONS);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, @NonNull List<String> perms) {
+
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, @NonNull List<String> perms) {
+        Toast.makeText(this, "Need permission to run this app", Toast.LENGTH_LONG).show();
+    }
+}
